63b5cde88e4d443ab154d5893e10aa3928d6b133
Yonathan Gutierrez <ygutierrez@cognos.com.co>
2022-01-03 15:17:54 -0500

    changes in form add css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
